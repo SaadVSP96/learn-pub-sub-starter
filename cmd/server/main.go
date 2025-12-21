57b84063646ba51e6ef2221d53dc18deb7807f23
SaadVSP96 <saadmasood192@gmail.com>
2025-12-21 18:23:17 +0500

    done a try - didnt work - trying fixes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connString := "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("connection to rabbitmq server failed: %v", err)
+	}
+	defer connection.Close()
+
+	channel, err := connection.Channel()
+	if err != nil {
+		log.Fatalf("generating channel from the connection failed: %v", err)
+	}
+	err = pubsub.PublishJSON(channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true})
+
+	fmt.Println("RabbitMQ connection successful...")
+
+	// Listen for Ctrl+C
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	<-signalChan
+	fmt.Println("Shutting down Peril server...")
 }
