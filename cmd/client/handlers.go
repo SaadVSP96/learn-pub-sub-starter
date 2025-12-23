ba70ed8cfc9217c780c91c42b4f2203163c113fe
SaadVSP96 <saadmasood192@gmail.com>
2025-12-23 21:04:58 +0500

    Bootdev code added though was the same as mine, actual issue was deleting and redeclaring game_logs queue with updated settings

--- cmd/client/handlers.go
+++ cmd/client/handlers.go
@@ -12,10 +12,11 @@ import (
 func handlerMove(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogic.ArmyMove) pubsub.Acktype {
 	return func(move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Print("> ")
+
 		moveOutcome := gs.HandleMove(move)
 		switch moveOutcome {
 		case gamelogic.MoveOutcomeSamePlayer:
-			return pubsub.NackDiscard
+			return pubsub.Ack
 		case gamelogic.MoveOutComeSafe:
 			return pubsub.Ack
 		case gamelogic.MoveOutcomeMakeWar:
@@ -34,28 +35,31 @@ func handlerMove(gs *gamelogic.GameState, publishCh *amqp.Channel) func(gamelogi
 			}
 			return pubsub.NackRequeue
 		}
+
 		fmt.Println("error: unknown move outcome")
 		return pubsub.NackDiscard
 	}
 }
 
-func handlerWar(gs *gamelogic.GameState) func(gamelogic.RecognitionOfWar) pubsub.Acktype {
+func handlerWar(gs *gamelogic.GameState) func(dw gamelogic.RecognitionOfWar) pubsub.Acktype {
 	return func(dw gamelogic.RecognitionOfWar) pubsub.Acktype {
 		defer fmt.Print("> ")
 		warOutcome, _, _ := gs.HandleWar(dw)
 		switch warOutcome {
 		case gamelogic.WarOutcomeNotInvolved:
-			return pubsub.NackDiscard
+			return pubsub.NackRequeue
 		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
 			return pubsub.Ack
 		case gamelogic.WarOutcomeYouWon:
 			return pubsub.Ack
 		case gamelogic.WarOutcomeDraw:
 			return pubsub.Ack
-		default:
-			fmt.Println("unrecongised result encountered")
-			return pubsub.NackDiscard
 		}
+
+		fmt.Println("error: unknown war outcome")
+		return pubsub.NackDiscard
 	}
 }
 
